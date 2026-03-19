3aafb99f06df25f6f3ffab5ef9c1421d85504c60
Hridrita <moonsaha721@gmail.com>
2026-03-20 00:21:10 +0600

    added axios and bar chart

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import './App.css'
 import Navbar from './components/DaisyNav/Navbar/Navbar'
 import PricingOptions from './components/DaisyNav/PricingOptions/PricingOptions'
 import ResultChart from './components/DaisyNav/ResultChart/ResultChart';
+import axios from 'axios';
+import MarksChart from './components/DaisyNav/MarksChart/MarksChart';
 
 
 const pricingPromise = fetch('PricingData.json')
 .then(res => res.json());
+
+const marksPromise = axios.get('marksData.json');
 
 function App() {
   return (
@@ -17,10 +21,14 @@ function App() {
 
       <main>
         
-        <Suspense fallback={<div className="text-center font-bold text-2xl">Loading Pricing...</div>}>
+        <Suspense fallback={<span className="text-center font-bold text-2xl">Loading Pricing...</span>}>
           <PricingOptions pricingPromise={pricingPromise} />
         </Suspense>
       </main>
+
+      <Suspense fallback={<span className="text-center font-bold text-2xl">Loading Pricing...</span>}>
+        <MarksChart marksPromise={marksPromise}></MarksChart>
+      </Suspense>
 
       <ResultChart></ResultChart>
     </>
