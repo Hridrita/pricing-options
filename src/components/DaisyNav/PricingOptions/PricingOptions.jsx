1807dbb6a7cd3426cf56569847d8b68f431a5771
Hridrita <moonsaha721@gmail.com>
2026-03-19 20:48:48 +0600

    modified

--- src/components/DaisyNav/PricingOptions/PricingOptions.jsx
+++ src/components/DaisyNav/PricingOptions/PricingOptions.jsx
@@ -1,26 +1,18 @@
-import React, { use } from 'react';
-import PricingCard from '../PricingCard/PricingCard';
-import DaisyPricing from '../DaisyPricing/DaisyPricing';
-
 const PricingOptions = ({pricingPromise}) => {
-
-    const pricingdata  = use(pricingPromise);
-    console.log(pricingdata);
+    const pricingdata = use(pricingPromise);
 
     return (
-        <div>
-            <h2 className='text-5xl'>Get Our membership</h2>
-            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
-                {/* {
-                    pricingdata.map(pricing => <PricingCard key={pricing.id} pricing={pricing}></PricingCard>)
-                } */}
-
+        <div className="p-10">
+            <h2 className='text-5xl text-center mb-10'>Get Our Membership</h2>
+            
+            {/* items-stretch নিশ্চিত করে যে সব কার্ডের উচ্চতা সমান হবে */}
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8 items-stretch justify-items-center'>
                 {
-                    pricingdata.map(pricing => <DaisyPricing key={pricing.id} pricing={pricing}></DaisyPricing>)
+                    pricingdata.map(pricing => (
+                        <DaisyPricing key={pricing.id} pricing={pricing}></DaisyPricing>
+                    ))
                 }
             </div>
         </div>
     );
 };
-
-export default PricingOptions;
